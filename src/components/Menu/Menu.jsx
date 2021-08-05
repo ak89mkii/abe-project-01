111d8d9b56e7f223568f88a1a03a1054d1e1d3bc
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-08-04 22:09:15 -0400

    Content and Index pages.

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Divider, Menu } from 'semantic-ui-react'
+import { Divider, Menu, Dropdown, Grid } from 'semantic-ui-react'
 import '../../App.css';
 import Main from '../../img/01.png'
 
-// Revisit: Need to figure out "fetch" for Flask backend.
 class Activator extends Component {
     state = {
         activeItem: undefined
@@ -16,8 +15,71 @@ class Activator extends Component {
         const { activeItem } = this.state
         return (
             <div className="color">
-                <br></br>                    
-                <img className="photo" src={Main} />
+                <br></br>   
+                <Grid centered columns={2}>
+                    <Grid.Row>
+                        <Grid.Column>           
+                            <img className="photo" src={Main} />
+                        </Grid.Column> 
+                        <Grid.Column>        
+                            <Menu secondary>
+                                {/* Services */}
+                                <Dropdown item text='Services'>
+                                    <Dropdown.Menu>
+                                        {/* For Individuals & Families  */}
+                                        <Dropdown.Item>
+                                            <Dropdown text='For Individuals & Families'>
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item>
+                                                        Will & Estate Services
+                                                    </Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                        </Dropdown.Item>
+                                        <Dropdown.Item>
+                                            <Dropdown text='For Businesses'>
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item>
+                                                        Launch Business or Nonprofit
+                                                    </Dropdown.Item>
+                                                    <Dropdown.Item>
+                                                        Business Owner Will & Succession
+                                                    </Dropdown.Item>
+                                                    <Dropdown.Item>
+                                                       Grant & Loan Help
+                                                    </Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                        </Dropdown.Item>
+                                        <Dropdown.Item>For Lawyers</Dropdown.Item>
+                                        <Dropdown.Item>For Employers</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                                <Menu.Item
+                                name='team'
+                                active={activeItem === 'messages'}
+                                onClick={this.handleItemClick}
+                                />
+                                <Menu.Item
+                                name='about us'
+                                active={activeItem === 'friends'}
+                                onClick={this.handleItemClick}
+                                />
+                                <Menu.Item
+                                name='contact'
+                                active={activeItem === 'messages'}
+                                onClick={this.handleItemClick}
+                                />
+                                <Menu.Item
+                                name='home'
+                                active={activeItem === 'friends'}
+                                onClick={this.handleItemClick}
+                                />
+
+                            </Menu>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
                 <Divider></Divider>
             </div>
         )
